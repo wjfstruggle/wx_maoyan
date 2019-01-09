bc8bae5ddd3db3c88041c309ef915a48f31ae546
wjfstruggle <2399012862@qq.com>
2019-01-09 20:46:12 +0800

    猫眼电影

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,11 @@
 const baseUrl = 'http://m.maoyan.com/ajax/'
 
 const api = {
-    movieList: baseUrl + 'movieOnInfoList' // 电影首页接口
+    movieList: baseUrl + 'movieOnInfoList', // 电影首页接口
+    moreComingList: baseUrl + 'moreComingList', // 电影首页加载更多信息
+    mostExpected: baseUrl + 'mostExpected', // 最近受期待电影
+    comingList: baseUrl + 'comingList', // 电影列表
+    moreComingList_up: baseUrl + 'moreComingList' // 更多电影列表
 }
 
 module.exports = api;
